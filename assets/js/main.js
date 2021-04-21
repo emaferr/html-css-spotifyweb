98cc8679e2da363899bad27278ec7df1a60f221e
ema.ferr <78190534+emaferr@users.noreply.github.com>
2021-04-21 17:54:44 +0200

    Aggiunto file .js collegato lo stesso eseguito l'esercizio

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+// Use document writeln (Nomino il creatore della pagina)
+document.writeln("This page was created by Emanuele #Classe35");
+
+// Use alert (Avviso che l'utente e bannato)
+alert('Your account has been banned from Spotify Web!');
+
+// use getElementById (Cambio il testo all'interno del button upgrade in log in e relativi colori)
+document.getElementById('btn_upgrade').innerHTML = ("sign in");
+document.getElementById('btn_upgrade').style.backgroundColor = "white";
+document.getElementById('btn_upgrade').style.color = "black";
+
+    
