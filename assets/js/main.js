1d6aad9c9d70fe98ba06306d5bd60336791cb143
ema.ferr <78190534+emaferr@users.noreply.github.com>
2021-04-21 17:55:21 +0200

    Fix error text

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ document.writeln("This page was created by Emanuele #Classe35");
 // Use alert (Avviso che l'utente e bannato)
 alert('Your account has been banned from Spotify Web!');
 
-// use getElementById (Cambio il testo all'interno del button upgrade in log in e relativi colori)
+// use getElementById (Cambio il testo all'interno del button upgrade in sign in e relativi colori)
 document.getElementById('btn_upgrade').innerHTML = ("sign in");
 document.getElementById('btn_upgrade').style.backgroundColor = "white";
 document.getElementById('btn_upgrade').style.color = "black";
